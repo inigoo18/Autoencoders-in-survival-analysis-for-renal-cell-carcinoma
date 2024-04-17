841ca50c07663c9f86b4706cce69f7c5e6e109a7
Inigo18 <123544770+inigoo18@users.noreply.github.com>
2024-04-17 18:19:25 +0200

    graph kfold

--- Implementation/Logic/Main.py
+++ Implementation/Logic/Main.py
@@ -59,13 +59,13 @@ def tabular_network(BATCH_SIZE, L, loss_args, clinicalVars, EPOCHS, FOLDS):
 
 
 
-def graph_network(BATCH_SIZE, L, loss_args, clinicalVars, EPOCHS):
+def graph_network(BATCH_SIZE, L, loss_args, clinicalVars, EPOCHS, FOLDS):
     current_directory = os.getcwd()
     somepath = os.path.abspath(
         os.path.join(current_directory, '..', '..', 'Data', 'RNA_dataset_graph_R3.pkl'))
 
     d = GraphDataLoader(somepath, ['PFS_P', 'PFS_P_CNSR'], clinicalVars, 0.2, 0.1,
-                          BATCH_SIZE)  # 70% train, 20% test, 10% val
+                          BATCH_SIZE, FOLDS)  # 70% train, 20% test, 10% val
 
     losses = [LossType.DENOISING, LossType.SPARSE_KL] #LossType.VARIATIONAL
 
@@ -79,33 +79,36 @@ def graph_network(BATCH_SIZE, L, loss_args, clinicalVars, EPOCHS):
     if losses not in combinations:
         combinations += [losses]
 
-    instanceModels = []
-
     combinations = [[]]
 
-    print("Input dim:", d.input_dim())
+    foldObjects = []
+
     for comb in combinations:
         print(comb)
-        title = "GPH{{L_" + str(L) + "_" + '+'.join(str(loss.name) for loss in comb)
-        loss_fn = LossHandler(comb, loss_args, d.adjacency_matrix)
-        aeModel = GNNExample(1, d.input_dim(), L, BATCH_SIZE)
-        vaeModel = VariationalExample(d.input_dim(), L)
-        if LossType.VARIATIONAL in comb:
-            aeModel = vaeModel
-        optim = torch.optim.Adam(aeModel.parameters(), lr=0.0005)
-        instanceModel = TrainingModel(title, d, clinicalVars,
-                                      aeModel, loss_fn, optim, EPOCHS, BATCH_SIZE,
-                                      L, True)#, 'model_lossGRAPH_L_256__182.pth')
-        instanceModels += [instanceModel]
+        foldObject = FoldObject(comb, FOLDS, d.allDatasets)
+        for fold in range(FOLDS):
+            title = "GPH{{L_" + str(L) + "_F_" + str(fold) + "_" + '+'.join(str(loss.name) for loss in comb)
+            loss_fn = LossHandler(comb, loss_args, None)
+            aeModel = GNNExample(1, d.input_dim, L, BATCH_SIZE)
+            vaeModel = VariationalExample(d.input_dim, L)
+            if LossType.VARIATIONAL in comb:
+                print("VARIATIONAL NOT YET IMPLEMENTED FOR GNNs!!!")
+                #aeModel = vaeModel
+            optim = torch.optim.Adam(aeModel.parameters(), lr=0.0001)
+            instanceModel = TrainingModel(title, d, foldObject.iterations[fold], clinicalVars,
+                                          aeModel, loss_fn, optim, EPOCHS, BATCH_SIZE, L,
+                                          True)  # , 'best_model_loss_1478.pth')
 
-    trainer = Trainer(instanceModels)
-    trainer.trainAll()
-    trainer.evaluateAll()
-
+            trainer = Trainer(instanceModel)
+            trainer.train()
+            meanRes, mseError = trainer.evaluate()
+            foldObject.MSE += [mseError]
+            foldObject.ROC += [meanRes]
+        foldObjects += [foldObject]
 
 
 if __name__ == "__main__":
-    option = "Tabular"
+    option = "Graph"
 
     torch.manual_seed(42)
     np.random.seed(42)
