e357cb73e7d5ecba20935e8dddc0f4687d9a0501
Inigo18 <123544770+inigoo18@users.noreply.github.com>
2024-05-05 13:08:07 +0200

    another checkpoint

--- Implementation/Logic/Main.py
+++ Implementation/Logic/Main.py
@@ -287,7 +287,7 @@ def convert_to_excel(names, ys, typename, L, FOLDS, COHORTS, pvalues_cohort, pva
 
 
 if __name__ == "__main__":
-    option = "Tabular"
+    option = "Graph"
     WITH_HISTOLOGY = False
 
     torch.manual_seed(42)
@@ -297,7 +297,7 @@ if __name__ == "__main__":
     loss_args = {'noise_factor': 0.01, 'reg_param': 0.4, 'rho': 0.01}
     clinicalVars = ['MATH', 'HE_TUMOR_CELL_CONTENT_IN_TUMOR_AREA', 'PD-L1_TOTAL_IMMUNE_CELLS_PER_TUMOR_AREA',
                     'CD8_POSITIVE_CELLS_TUMOR_CENTER', 'CD8_POSITIVE_CELLS_TOTAL_AREA']
-    EPOCHS = 80
+    EPOCHS = 50
     FOLDS = 10
     COHORTS = ['Avelumab+Axitinib','Sunitinib'] # ['Avelumab+Axitinib'] # ['ALL','Avelumab+Axitinib','Sunitinib']
 
